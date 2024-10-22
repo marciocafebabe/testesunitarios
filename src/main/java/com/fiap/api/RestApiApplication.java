cb0d7cebf16c0e23a62360b37e888e15f0e083de
Marcio Cintra <cintramaf@gmail.com>
2024-10-22 18:34:52 -0300

    commit test

--- src/main/java/com/fiap/api/RestApiApplication.java
+++ src/main/java/com/fiap/api/RestApiApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RestApiApplication {
-    
+public class RestApiApplication {   
     public static void main(String[] args) {
         SpringApplication.run(RestApiApplication.class, args);
     }
-
 }
